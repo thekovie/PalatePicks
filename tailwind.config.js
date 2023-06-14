87315e9a363089d546c7ae397ca6dc170e155b50
John Kovie Nino <thekovie@gmail.com>
2023-06-14 19:30:52 +0800

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,44 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      transparent: 'transparent',
+      white : '#FEFEFF',
+      grey: '#7E8581',
+      black : '#000000',
+      green: '#4BAD70',
+      green_light: '#E8F2E7', // All Reviews Button
+      green_dark: '4B8D59',
+      green_lightbg: '#EDFCF4', // Per Restaurant box in home/explore page and modals
+      yellow: '#FFE500',
+      red: '#FF3232',
+    },
+    fontFamily: {
+      'sans': ['Wix Madefor Text', 'sans-serif'],
+      'cursive': ['Comfortaa', 'cursive'],
+    },
+    extend: {
+      spacing: {
+
+      },
+      borderRadius: {
+
+      }
+    },
   },
   plugins: [],
 }
