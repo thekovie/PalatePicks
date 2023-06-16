cc19d8c3cbbabd5c7439f94c62866fda1b8a9fa4
Kovie <thekovie@gmail.com>
2023-06-16 18:32:47 +0800

    Add navbar and footer

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <template>
-  <div id="header">
-    <h1 class="logo">PalatePicks</h1>
-    <router-link to="/">Home</router-link>
-    <router-link :to="{ name: 'Explore'}">Explore  </router-link>
-    <router-link :to="{ name: 'Register'}">Login / Register</router-link>
+  <div class="header fixed top-0 flex flex-wrap justify-between items-center text-green flex-row p-5 border-b w-screen   border-solid">
+    <div class="left flex flex-row items-center">
+      <router-link class="logo font-cursive normal-case font-bold text-3xl justify-start" to="/">PalatePicks</router-link>
+      <div class="nav-menu ml-6 space-x-9">
+        <router-link to="/">Home</router-link>
+        <router-link :to="{ name: 'Explore'}">Explore  </router-link>
+      </div>
+    </div>
+    <button class="right bg-green_light p-3 rounded-full flex">
+      <router-link class=" text-xs uppercase" :to="{ name: 'Register'}">Login / Register</router-link>
+    </button>
   </div>
 
   <router-view />
 
-  <div id="footer">
-    <p>Footer</p>
+  <div class="footer fixed w-screen flex justify-between bg-green h-32 bottom-0 items-center p-8 pr-20 pl-20 text-white">
+    <div class="left justify-start">
+      <div class="logo font-cursive normal-case font-bold text-3xl">PalatePicks</div>
+      <div class="font-light">Fueling Student Appetites, One Bite at a Time</div>
+    </div>
+    <div class="right text-right text-xs justify-end">
+      <p>© CCAPDEV - S13 Group 3</p>
+      <p>2401 Taft Ave, Malate, Manila,</p>
+      <p>1004 Metro Manila,</p>
+      <p>Philippines</p>
+    </div>
   </div>
 </template>
 
 <style scoped>
-  #header {
+  .header {
     text-transform: uppercase;
-  }
-
-  .logo {
-    font-family: 'Comfortaa', cursive;
-    text-transform: none;
   }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,6 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    },
     colors: {
       transparent: 'transparent',
       white : '#FEFEFF',
