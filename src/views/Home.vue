9c7d47ac05c5123c08c1c900df7f73202495df34
Kovie <thekovie@gmail.com>
2023-06-18 22:39:39 +0800

    Update search bar display

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,9 @@
       <div class="text-center text-3xl font-bold text-white">
           Hi, Juan! What are you craving lately?
       </div>
-      <div class="search-center-box mt-4 flex relative">
+      <div class="search-center-box mt-4 flex">
         <input type="text" class="search-bar w-[600px] bg-white h-14 rounded-s-3xl pl-4 pr-4 focus:outline-none" placeholder="Search restaurant here" />
-        <div class="search-btn relative w-16 h-14 pr-10 rounded-e-3xl pt-4 bg-white items-center">
+        <div class="search-btn w-16 h-14 pr-10 rounded-e-3xl pt-4 bg-white items-center">
           <img class="w-5 h-5 bg-white" src="../assets/Search.svg" alt="search" />
         </div>
       </div>
