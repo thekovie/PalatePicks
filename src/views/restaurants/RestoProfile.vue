b96468aae7a5040b942dae3dc4be224a207f4ec9
John Kovie Nino <thekovie@gmail.com>
2023-06-19 18:40:35 +0800

    Update RestoProfile.vue

--- src/views/restaurants/RestoProfile.vue
+++ src/views/restaurants/RestoProfile.vue
@@ -1,5 +1,5 @@
 <template>
-  <body class="min-h-screen">
+  <body class="min-h-screen pb-80">
     <div class="cover-page h-[586px] min-w-screen flex flex-col pl-56 pr-64 justify-center text-white">
       <div class="resto-title font-bold text-5xl">
         {{ resto }}
