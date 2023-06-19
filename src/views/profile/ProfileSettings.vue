6c499f25041cd61d42b08b07ea15a643313efde9
ReignLarraquel <reignlarraquel@gmail.com>
2023-06-19 15:26:48 +0800

    Add Profile Bar

--- src/views/profile/ProfileSettings.vue
+++ src/views/profile/ProfileSettings.vue
@@ -1,3 +1,18 @@
 <template>
+  <div class="mx-20 my-10"> <!--Content Section-->
+    <div class="flex flex-row items-center"> <!--Profile Section-->
+      <div> <!--Avatar-->
+        <img src="../../assets/images/user.jpg" alt="Avatar" class="w-12 h-12 rounded-full me-3"/>
+      </div>
+      <div class="basis-3/4">  <!--User-->
+        <p class="font-bold text-xl">@Tofudubu (Kovie Niño)</p>
+        <p>Your Personal Account</p>
+      </div>
+      <div class="basis-1/4 text-right pe-20"> <!--Go back to personal profile-->
+        <p class="underline">Go Back to Personal Profile</p>
+      </div>
+    </div>
 
+
+  </div>
 </template>
