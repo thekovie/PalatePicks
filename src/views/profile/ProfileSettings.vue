bf2833e94bc1330523e80d09ee0e5cdbe7589e9c
ReignLarraquel <reignlarraquel@gmail.com>
2023-06-20 02:00:44 +0800

    Fix Issues in ProfileSettings
    * Change placeholder to value
    * Add padding to textboxes
    * Add padding to buttons
    * Set send code button to disable
    * Set update email to disable
    
    Co-authored-by: ReddMA <ReddMA@users.noreply.github.com>

--- src/views/profile/ProfileSettings.vue
+++ src/views/profile/ProfileSettings.vue
@@ -36,12 +36,12 @@
 
                 <div class="basis-1/2 mt-5 me-10">
                   <label>First Name<br></label>
-                  <input type="text" placeholder="Kovie" class="border-green border rounded w-full" v-model="firstName">
+                  <input type="text" value="Kovie" class="border-green border rounded w-full px-2" @click="clickTextBox">
                 </div>
 
                 <div class="basis-1/2 mt-5">
                   <label>Last Name<br></label>
-                  <input type="text" placeholder="Niño" class="border-green border rounded w-full" v-model="lastName">
+                  <input type="text" value="Niño" class="border-green border rounded w-full  px-2" @click="clickTextBox">
                 </div>
 
               </div>
@@ -50,7 +50,7 @@
 
                 <div class="mt-5"> <!--Username-->
                   <label>Username<br></label>
-                  <input type="text" placeholder="@Tofudubu" class="border-green border rounded w-full" v-model="username">
+                  <input type="text" value="@Tofudubu" class="border-green border rounded w-full  px-2" @click="clickTextBox">
                 </div>
 
               </div>
@@ -59,7 +59,7 @@
 
                 <div class="mt-5"> <!--School-->
                   <label>School/University<br></label>
-                  <input type="text" placeholder="De La Salle University" class="border-green border rounded w-full" v-model="school">
+                  <input type="text" value="De La Salle University" class="border-green border rounded w-full  px-2" @click="clickTextBox">
                 </div>
 
               </div>
@@ -68,18 +68,18 @@
 
             <div class="flex flex-col place-items-center mt-5 basis-1/4"> <!--Avatar-->
               <img src="../../assets/images/user.jpg" alt="Avatar" class="w-36 h-36 rounded-full me-3"/>
-              <button class="bg-green text-white px-7 mx-14 mt-5 rounded-3xl">Change Avatar</button>
+              <button class="bg-green text-white px-7 mx-14 mt-5 py-1 rounded-3xl">Change Avatar</button>
             </div>
 
           </div>
 
           <div class="mt-5"> <!--Bio-->
             <label>Bio<br></label>
-            <textarea rows="4" class="me-10 ps-2 border-green border rounded w-full" placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus iaculis urna id volutpat lacus laoreet non curabitur. Dui faucibus in ornare quam viverra orci sagittis eu volutpat. Ultrices mi tempus imperdiet nulla malesuada." v-model="school">
+            <textarea rows="4" @click="clickTextBox" class="border-green border rounded w-full px-2" value="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus iaculis urna id volutpat lacus laoreet non curabitur. Dui faucibus in ornare quam viverra orci sagittis eu volutpat. Ultrices mi tempus imperdiet nulla malesuada.">
             </textarea>
           </div>
 
-          <button class="bg-green text-white px-12  mt-5 rounded-3xl">Save Changes</button>
+          <button class="bg-green text-white px-12  mt-5 py-1 rounded-3xl">Save Changes</button>
         </form>
       </div>
 
@@ -91,13 +91,13 @@
 
         <form @submit.prevent="saveChanges">
           <div class="flex">
-            <div class="basis-3/4 mt-5">
+            <div class="basis-4/5 mt-5">
               <label>Email<br></label>
-              <input type="text" placeholder="kovie_nino@dlsu.edu.ph" class="border-green border rounded w-full" v-model="email" required>
+              <input type="text" value="kovie_nino@dlsu.edu.ph" class="border-green border rounded w-full px-2" required @click="clickTextBox">
             </div>
 
-            <div class="mt-10 mx-14 bg-green text-white px-12 rounded-3xl">
-              <button> Send Code</button>
+            <div class="mt-10 ms-10 bg-green text-white px-12 rounded-3xl">
+              <button class="py-1" :disabled="isDisabled"> Send Code</button>
             </div>
 
           </div>
@@ -106,14 +106,14 @@
 
             <div class="mr-20 mt-5">
               <label>Code<br></label>
-              <input type="text" placeholder="000 - 000" class="border-green border rounded w-full" v-model="code">
-              <p class="opacity-60 my-2">Insert the 6 digit code sent to your email to confirm the change of email</p>
+              <input :disabled="isDisabled" type="text" value="000 - 000" class="border-green border rounded w-full px-2" @click="clickTextBox">
+              <p class="opacity-60 text-xs my-2">Insert the 6 digit code sent to your email to confirm the change of email</p>
             </div>
 
           </div>
 
           <div>
-            <button class="bg-green text-white px-12  mt-5 rounded-3xl">Update Email</button>
+            <button class="bg-green text-white px-12  mt-5 py-1 rounded-3xl">Update Email</button>
           </div>
         </form>
       </div>
@@ -129,7 +129,7 @@
 
             <div class="mr-20 mt-5">
               <label>Current Password<br></label>
-              <input type="password" placeholder="**********" class="border-green border rounded w-full" v-model="currentPassword" required>
+              <input type="password" value="**********" class="border-green border rounded w-full px-2" required @click="clickTextBox">
             </div>
 
           </div>
@@ -140,20 +140,20 @@
 
               <div class="basis-1/2 mt-5 me-10">
                 <label>New Password<br></label>
-                <input type="password" placeholder="************" class="border-green border rounded w-full" v-model="newPassword">
+                <input type="password" value="************" class="border-green border rounded w-full px-2" required @click="clickTextBox">
               </div>
 
               <div class="basis-1/2 mt-5">
                 <label>Confirm New Password<br></label>
-                <input type="password" placeholder="************" class="border-green border rounded w-full" v-model="confirmNewPassword">
+                <input type="password" value="************" class="border-green border rounded w-full px-2" required @click="clickTextBox">
               </div>
             </div>
 
           </div>
-          <p class="opacity-60 my-2">Make sure it's at least 15 characters OR at least 8 characters including a number and a lowercase letter.</p>
+          <p class="opacity-60 text-xs my-2">Make sure it's at least 15 characters OR at least 8 characters including a number and a lowercase letter.</p>
 
           <div>
-            <button class="bg-green text-white px-8  mt-5 rounded-3xl">Update Password</button>
+            <button class="bg-green text-white px-8 py-1 mt-5 rounded-3xl">Update Password</button>
           </div>
         </form>
       </div>
@@ -179,5 +179,19 @@ export default {
       this[property] = event.target.value;
     }
   },
+  data() {
+    return {
+      isDisabled: true
+    };
+  },
 }
 </script>
+
+<style scoped>
+input[type="text"],
+input[type="password"],
+textarea {
+  outline-color: green;
+}
+
+</style>
