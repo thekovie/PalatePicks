06b90bfe6fb7c86c8765fbf2552edb3cb2241b66
ReignLarraquel <reignlarraquel@gmail.com>
2023-06-21 23:40:36 +0800

    Add User Profile Page

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,3 +1,28 @@
 <template>
+<div class="justify-center flex flex-col items-center"> <!--Profile-->
 
+  <img src="../../assets/images/user.jpg" alt="Avatar" class="w-44 h-44 rounded-full"/>
+  <p class="text-green text-xl font-bold mt-5">Kovie Niño</p>
+  <p class="font-bold mt-1">@tofudubu</p>
+  <p class="mt-1">De La Salle University</p>
+
+  <div class="max-w-3xl text-center mt-1">
+
+    <p class="line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis facilisis elit et purus gravida tristique. Mauris vitae mattis ante. Vestibulum condimentum, justo a bibendum lacinia.</p>
+
+  </div>
+
+  <button class="bg-green text-white px-12 mt-4 mb-4 py-1 rounded-3xl" v-if="isUser"><router-link :to="{ name: 'ProfileSettings'}">Edit Profile</router-link></button>
+</div>
+<!--Component Here-->
 </template>
+
+<script>
+export default {
+  data() {
+      return {
+        isUser: true,
+      }
+    },
+}
+</script>
