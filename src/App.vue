44255d1af53ee1e2920b9ec7187f72d54be2e4f4
Kovie <thekovie@gmail.com>
2023-06-22 19:03:54 +0800

    Revert "Merge pull request #29 from thekovie/profile-settings-page"
    This reverts commit 8a29cd71bda19525858bfdfe6025740241a328ea, reversing
    changes made to bcba31ec860fbb964d90075aad0fa37484c46b35.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="header sticky top-0 flex flex-wrap justify-between items-center bg-white text-green flex-row p-5 border-b w-screen border-solid">
+  <div class="header sticky top-0 flex flex-wrap justify-between items-center bg-white text-green flex-row p-5 border-b min-w-screen border-solid">
     <div class="left flex flex-row items-center">
       <router-link class="logo font-cursive normal-case font-bold text-3xl justify-start" to="/">PalatePicks</router-link>
       <div class="nav-menu ml-6 space-x-9">
@@ -7,13 +7,12 @@
         <router-link :to="{ name: 'Explore'}">Explore  </router-link>
       </div>
     </div>
-    <NavUser v-if="!isLoggedIn"/>
-    <NavUserProfile v-else="isLoggedIn"/>
+    <NavUser></NavUser>
   </div>
 
   <router-view />
 
-  <div class="footer w-screen flex justify-between bg-green h-32 bottom-0 items-center p-8 pr-20 pl-20 text-white">
+  <div class="footer min-w-screen flex justify-between bg-green h-32 bottom-0 items-center p-8 pr-20 pl-20 text-white">
     <div class="left justify-start">
       <div class="logo font-cursive normal-case font-bold text-3xl">PalatePicks</div>
       <div class="font-light">Fueling Student Appetites, One Bite at a Time</div>
@@ -29,21 +28,10 @@
 
 <script>
  import NavUser from './components/NavUser.vue'
- import NavUserProfile from './components/NavUserProfile.vue'
 
   export default {
     components: {
-      NavUser, NavUserProfile
-    },
-    data() {
-      return {
-        isLoggedIn: true,
-      }
-    },
-    methods: {
-      logout() {
-        this.isLoggedIn = false
-      }
+      NavUser
     }
   }
 </script>
