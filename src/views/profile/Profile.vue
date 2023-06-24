3d10f592ef9f6b8872c94dcee5f6ef9862b805f7
Kovie <thekovie@gmail.com>
2023-06-24 16:19:37 +0800

    Modify user json and profile design

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,30 +1,20 @@
 <template>
-
-
+  <div class="min-h-screen mt-24">
     <div class="profile justify-center flex flex-col items-center"> <!--Profile-->
-    <img :src="Profile.profileImgSrc" alt="Avatar" class="w-44 h-44 rounded-full mt-5 object-center object-cover border-green border-[3px]"/>
-    <p class="text-green text-xl font-bold mt-5"></p>
-    <p class="font-bold mt-1">@{{ Profile.username }}</p>
-    <p class="mt-1">{{ Profile.school }}</p>
-    <p class="max-w-3xl text-center mt-1">{{ Profile.bio }}</p>
-    <button class="bg-green text-white px-12 mt-4 mb-4 py-1 rounded-3xl" v-if="isUser"><router-link :to="{ name: 'ProfileSettings'}">Edit Profile</router-link></button>
-
+      <img :src="Profile.profileImgSrc" alt="Avatar" class=" w-48 h-48 rounded-full mt-5 object-center object-cover border-green border-[3px]"/>
+      <p class="text-green text-3xl font-bold mt-5">{{ Profile.firstName }} {{ Profile.lastName }}</p>
+      <p class="font-medium mt-1 text-xl">@{{ Profile.username }}</p>
+      <p class="mt-1 text-lg">{{ Profile.school }}</p>
+      <p class="max-w-3xl text-center text-base mt-1">{{ Profile.bio }}</p>
+      <button class="bg-green text-white px-12 mt-4 py-1 rounded-3xl" v-if="isUser"><router-link :to="{ name: 'ProfileSettings'}">Edit Profile</router-link></button>
     </div>
-
-    <div class="user-reviews px-20">
+    <div class="user-reviews px-20 mt-4">
       <p class="font-bold p-3">View {{ Profile.firstName }}'s Reviews</p>
-
       <div class="reviews-list flex flex-col gap-8">
         <UserReview class="mb-6" />
       </div>
-
     </div>
-
-
-
-
-
-
+  </div>
 </template>
 
 <script>
