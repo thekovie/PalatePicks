dce09554743cfe973e7bc71618d11e34509c8a9f
ReignLarraquel <reignlarraquel@gmail.com>
2023-07-20 14:04:57 +0800

    Change filename

---
+++ pages/search/[id].vue
@@ -0,0 +1,21 @@
+<template>
+  <Preloader v-if="loading" :loading="loading" />
+  <div class="min-h-screen mb-24">
+    <h1>Search results for</h1>
+    <p>"{{ id }}"</p>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      session: Object,
+      loggedInUser: String,
+      loggedUserProfile: Array
+    }
+  }
+  const { id } = useRoute().params
+</script>
