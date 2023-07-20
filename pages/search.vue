d3076cfcd69bf38723634488d3fb8d0f81cee130
ReignLarraquel <reignlarraquel@gmail.com>
2023-07-21 01:12:03 +0800

    add 0 result functionality and fix button

--- pages/search.vue
+++ pages/search.vue
@@ -4,12 +4,23 @@
     <div class="mx-40 my-10">
       <h1 class="text-2xl font-bold">Search results for</h1>
       <p class="text-lg">"{{ search }}"</p>
-      <NuxtLink to="/">
-        <button class="back-button bg-green text-white rounded-3xl flex items-center font-light px-6 py-3 my-5">Back to Home</button>
-      </NuxtLink>
+
       <div class="mt-5 flex flex-wrap">
         <RestoBox v-for="establishment in establishments" :key="establishment.id" :imageHeader="establishment.imageHeader" :name="establishment.name" :description="establishment.description" :rating="establishment.rating" :price="establishment.price" />
       </div>
+      <div v-if="empty" class="flex flex-col items-center mt-20">
+          <img class="w-20 h-20" src="~/assets/icons/green-search.svg" alt="search" />
+          <p class="font-bold mt-5 text-xl">Sorry! No results found.</p>
+          <NuxtLink to="/">
+            <button class="back-button bg-green text-white rounded-3xl flex items-center font-light px-6 py-4 my-5">GO BACK TO HOME <img src="~/assets/icons/Arrow-left.svg" class="ms-5"/></button>
+          </NuxtLink>
+      </div>
+      <div v-else class="flex flex-col items-end">
+        <NuxtLink to="/">
+          <button class="back-button bg-green text-white rounded-3xl flex items-center font-light px-6 py-4 my-5">GO BACK TO HOME <img src="~/assets/icons/Arrow-left.svg" class="ms-5"/></button>
+        </NuxtLink>
+      </div>
+
     </div>
   </div>
 </template>
@@ -43,12 +54,18 @@
         supabase: useSupabaseClient(),
         search: useRoute().query.search,
         establishments: {},
+        empty: false,
         loading: true,
       }
     },
     async mounted(){
       await this.getRestaurant()
       console.log(this.establishments)
+
+      if(Object.keys(this.establishments).length === 0){
+        this.empty = true;
+      }
+
     }
   }
 </script>
