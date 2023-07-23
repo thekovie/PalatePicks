e70b0e2e4061fae040f64fbd9f390c40ea787cb0
Kovie <thekovie@gmail.com>
2023-07-23 20:13:27 +0800

    Remove button

--- pages/restaurant/[id].vue
+++ pages/restaurant/[id].vue
@@ -89,11 +89,6 @@
                   <option value="two-stars">2 Star Ratings</option>
                   <option value="one-star">1 Star Ratings</option>
                 </select>
-                <button class="filter-button absolute top-0 right-0 h-[50px] text-white rounded-r-3xl flex items-center  border-black px-6 py-3" @click="this.$ref.filterOptions.click()">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                  </svg>
-                </button>
               </div>
               <div class="reset-filters">
                 <button class="back-button bg-green text-white rounded-3xl text-center w-full font-light my-5 h-[45px]" @click="getReviews">
