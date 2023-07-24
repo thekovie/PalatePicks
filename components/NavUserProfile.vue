3a1691a31d4966ca79151b099c35b8efad5f118a
ReignLarraquel <reignlarraquel@gmail.com>
2023-07-24 14:12:38 +0800

    Fix NavUser dropdown size and router
    Co-authored-by: ReddMA <ReddMA@users.noreply.github.com>

--- components/NavUserProfile.vue
+++ components/NavUserProfile.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="nav-user-profile">
-    <button
-      :class="['center', 'bg-green_light', 'px-4', 'py-2', { 'rounded-3xl': !rounded, 'rounded-t-3xl': rounded }, 'flex', 'dropdown-button', { 'active': isDropdownOpen }]"
-      v-on:click="toggleDropdown"
-      ref="dropdownButton"
-    >
-      <img :src="loggedUserProfile[0].profile_img_src" alt="Avatar" class="center w-6 h-6 rounded-full mr-3 object-center object-cover"/>
-      <span>Hi, {{ loggedUserProfile[0].first_name }}!</span>
-    </button>
-    <div
-      class="bg-green_light px-3 rounded-b-2xl text-black dropdown-menu"
-      :class="{ 'show': isDropdownOpen }"
-      ref="dropdownMenu"
-    >
-      <router-link class="block text-sm px-2 py-2" :to="`/profile/${loggedUserProfile[0].username}`" @click="closeDropdown('click')">View Profile</router-link>
-      <router-link class="block text-sm px-2 py-2" to="/profile/settings" @click="closeDropdown('click')">Edit Profile</router-link>
-      <router-link class="block text-sm px-2 py-2 text-red" to="/" @click="closeDropdownAndLogout('click')">Logout</router-link>
+  <div class="nav-user-profile container mx-auto">
+    <div class="relative">
+      <button
+        :class="['center', 'bg-green_light', 'px-4', 'py-2', { 'rounded-3xl': !rounded, 'rounded-t-3xl': rounded }, 'flex', 'dropdown-button', { 'active': isDropdownOpen }]"
+        v-on:click="toggleDropdown"
+        ref="dropdownButton"
+      >
+        <img :src="loggedUserProfile[0].profile_img_src" alt="Avatar" class="center w-6 h-6 rounded-full mr-3 object-center object-cover"/>
+        <span>Hi, {{ loggedUserProfile[0].first_name }}!</span>
+      </button>
+      <div
+        :style="dropdownStyle"
+        :class="['bg-green_light', 'px-3', 'rounded-b-2xl', 'text-black', 'dropdown-menu', { 'show': isDropdownOpen }]"
+        ref="dropdownMenu"
+      >
+        <NuxtLink :to="`/profile/${loggedUserProfile[0].username}`" class="text-sm px-2 py-2" @click="closeDropdown('click')">View Profile<br></NuxtLink>
+        <NuxtLink to="/profile/settings" class="text-sm px-2 py-2" @click="closeDropdown('click')">Edit Profile<br></NuxtLink>
+        <NuxtLink to="/" class="text-sm px-2 py-2 text-red" @click="closeDropdownAndLogout('click')">Logout<br></NuxtLink>
+      </div>
     </div>
   </div>
 </template>
@@ -31,6 +33,17 @@ export default {
       isDropdownOpen: false,
       rounded: false,
     };
+  },
+  computed: {
+    dropdownStyle() {
+      if (this.isDropdownOpen) {
+        return {
+          width: `${this.$refs.dropdownButton.offsetWidth}px`,
+          left: `calc(100% - ${this.$refs.dropdownButton.offsetWidth}px)`,
+        };
+      }
+      return {};
+    },
   },
   methods: {
     toggleDropdown() {
@@ -56,15 +69,7 @@ export default {
       }
 
       this.$emit('logout');
-
     },
-
-    getProfileLink(username){
-      return `/profile/${username}`
-    }
-  },
-  async beforeUpdate(){
-
   },
   async mounted() {
     document.addEventListener('click', this.closeDropdown);
