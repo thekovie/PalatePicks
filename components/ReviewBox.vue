96659b4a7e910a7c66627d5ad42aededa9c9a6e8
ylsewlys <129646270+ylsewlys@users.noreply.github.com>
2023-07-31 22:20:34 +0800

    Fix media div icon overlay issue

--- components/ReviewBox.vue
+++ components/ReviewBox.vue
@@ -26,8 +26,10 @@
     </div>
     <div class="review-gallery flex flex-wrap md:flex-row mt-8">
       <div v-for="(media, index) in gallery" :key="index"  class="review-photo h-32 w-32 md:w-[150px] md:h-[150px] mr-6 mb-6 flex">
-        <img v-if="reviewFileTypeChecker(media)" class="w-full h-full object-cover flex mr-3 rounded-3xl cursor-pointer hover:filter hover:brightness-75" :src="media" alt="review photo" @click="toggleMediaView(media)"/>
-        <video v-else class="w-full h-full object-cover flex mr-3 rounded-3xl cursor-pointer hover:filter hover:brightness-75" :src="media" alt="review video" no-controls />
+        <div class="w-full">
+          <img v-if="reviewFileTypeChecker(media)" class="w-full h-full object-cover flex mr-3 rounded-3xl cursor-pointer hover:filter hover:brightness-75" :src="media" alt="review photo" @click="toggleMediaView(media)"/>
+          <video v-else class="w-full h-full object-cover flex mr-3 rounded-3xl cursor-pointer hover:filter hover:brightness-75" :src="media" alt="review video" no-controls />
+        </div>
         <div v-if="!reviewFileTypeChecker(media)" class="video-icon absolute bg-black bg-opacity-30 w-[150px] h-[150px] p-14 cursor-pointer rounded-3xl" @click="toggleMediaView(media)">
           <img class="w-full h-full" src="~/assets/icons/Video.svg" />
         </div>
