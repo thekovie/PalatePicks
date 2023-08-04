b0c32835ce54896105dfdbf4cc7f34a8317163a6
Kovie <thekovie@gmail.com>
2023-08-04 09:31:32 +0800

    Add Comment UX

--- components/FullReview.vue
+++ components/FullReview.vue
@@ -74,7 +74,10 @@
           </div>
 
           <!-- Write Comment Text Area-->
-          <textarea v-model="commentField" class="resize-y border-green border-[1px] rounded-[15px] w-[852px] px-[25px] py-[20px] mr-[18px]" placeholder="Write a comment..." maxlength="500"></textarea>
+          <div class="flex flex-col">
+            <textarea v-model="commentField" class="resize-y border-green border-[1px] rounded-[15px] w-[852px] px-[25px] py-[20px] mr-[18px]" placeholder="Write a comment..." maxlength="500" :class="{'focus:outline-red': commentField.length === 500}" required />
+            <div v-show="commentField.length === 500" class="text-red text-xs mt-2 ml-1">Comment must be less than or equal to 500 characters.</div>
+          </div>
 
           <!-- Send button -->
           <div class="bg-green min-h-[48px] min-w-[48px] max-h-[48px] max-w-[48px] rounded-full justify-self-end mt-[15px] flex cursor-pointer" @click="pushComment">
